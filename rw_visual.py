8b5e2c767b2c60ecb083c3c706132971706844d4
Scott <153109835+Username3533@users.noreply.github.com>
2024-02-24 00:01:52 +0100

    Add files via upload

---
+++ rw_visual.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+from random_walk import Randomwalk
+
+rw = Randomwalk()
+rw.fill_walk()
+
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s=15)
+plt.show()
